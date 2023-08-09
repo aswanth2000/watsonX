13457d926e892b60c1aaf2fba9da38bde8956fca
aswanth2000 <73633456+aswanth2000@users.noreply.github.com>
2023-08-09 15:45:43 +0530

    Update mail_sender.py

--- mail_sender.py
+++ mail_sender.py
@@ -3,8 +3,10 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email import encoders
+import ssl
 def send_mail(json_data):
-    sender = "watson_dragons@noreply.com"   
+    sender = "watsonx035@gmail.com" 
+    password = "akvfwflrjyehigfk"  
     msg = MIMEMultipart()
     msg['Subject'] = json_data['mail_subject']
     msg['From'] = sender
@@ -23,36 +25,12 @@ def send_mail(json_data):
     part.add_header('Content-Disposition', 'attachment; filename="%s"' % filename)
     msg.attach(part)
     print("Inside Mail Sender")
-    server = smtplib.SMTP('ap.relay.ibm.com')
+    context = ssl.create_default_context()
+    server = smtplib.SMTP('smtp.gmail.com' , 587)
+    server.starttls(context = context)
+    server.login(sender, password)
     for i in recipients:
         server.sendmail(msg['From'], i, msg.as_string())
     server.close()
     print("Mail sent")
     return "Mail Successfully went"
-def send_mail_get(mail_subject,mail_to,mail_body,name):
-    sender = "watson_dragons@noreply.com"   
-    msg = MIMEMultipart()
-    msg['Subject'] = mail_subject
-    msg['From'] = sender
-    recipients=[mail_to] 
-    msg['To'] = ", ".join(recipients)
-    msg.preamble = 'Multipart massage.\n'
-    message = mail_body
-    f = open(name+"-offerLetter.txt", "w")
-    f.write("Hi"+name+",\nWelcome to IBM. \nRegards, \nOnboarding Team")
-    f.close()
-    msg.attach(MIMEText(message))
-    filename =name+"-offerLetter.txt"
-    part = MIMEBase('application', "octet-stream")
-    part.set_payload(open(filename, "rb").read())
-    encoders.encode_base64(part)
-    part.add_header('Content-Disposition', 'attachment; filename="%s"' % filename)
-    msg.attach(part)
-    print("Inside Mail Sender")
-    server = smtplib.SMTP('ap.relay.ibm.com')
-    for i in recipients:
-        server.sendmail(msg['From'], i, msg.as_string())
-    server.close()
-    print("Mail sent")
-    return "Mail Successfully went"
-
