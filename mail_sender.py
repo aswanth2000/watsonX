c3da58f90184cf8553b8a0186d44a3c05771980a
aswanth2000 <73633456+aswanth2000@users.noreply.github.com>
2023-08-10 15:49:13 +0530

    Update mail_sender.py

--- mail_sender.py
+++ mail_sender.py
@@ -4,6 +4,8 @@ from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email import encoders
 import ssl
+import datetime
+
 def send_mail(json_data):
     sender = "watsonx035@gmail.com" 
     password = "akvfwflrjyehigfk"  
@@ -15,7 +17,8 @@ def send_mail(json_data):
     msg.preamble = 'Multipart massage.\n'
     message = json_data['mail_body']
     f = open(json_data['name']+"-offerLetter.txt", "w")
-    f.write("Dear "+json_data['name']+",\n We are pleased to extend an offer of employment for the position of "+json_data['position']+" at IBM. We were highly impressed with your qualifications and experience and are excited about the prospect of you joining our team. \nRegards, \nOnboarding Team")
+    today = datetime.date.today()
+    f.write("IBM India\nSA3,Banglore \n"+str(today)+"\n\nDear "+json_data['name']+",\n\n    We are pleased to extend an offer of employment for the position of "+json_data['position']+" at IBM. We were highly impressed with your qualifications and experience , and we believe that you will be a valuable addition to our team.\n\nJob Title: "+json_data['position']+"\nLocation:Banglore\n\nYour employment with IBM will be on a Full-Time basis. Please note that your employment is contingent upon successfully passing a [Background Chec, etc.], which is standard procedure for all new employees.We are excited about the prospect of having you on our team and believe that your skills and experience will contribute to our continued success. We look forward to your positive response and to welcoming you to the IBM family. \n\nRegards, \nOnboarding Team")
     f.close()
     msg.attach(MIMEText(message))
     filename =json_data['name']+"-offerLetter.txt"
